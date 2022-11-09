4f681c93e6a34f899903c0c7a0d8392bb0c4ce27
Nikhil Tiwari <69097170+nikhiltiwari008@users.noreply.github.com>
2022-11-09 14:28:54 +0530

    Add files via upload

---
+++ onedarray.java
@@ -0,0 +1,16 @@
+package Array;
+
+class onedarry{
+
+     
+    public static void main(String[] args) {
+       int[] a=new int[]{10,20,30,40,50,60,70,80,90};
+      
+      
+        for(int i=0; i<=a.length; i++){
+
+            System.out.println(a[i]);
+        }
+        
+    }
+}
