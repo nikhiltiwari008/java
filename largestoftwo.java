338c7121e24c1dcab0460ce154e2aa5b73a5de2b
Nikhil Tiwari <69097170+nikhiltiwari008@users.noreply.github.com>
2022-11-09 14:23:10 +0530

    Add files via upload

---
+++ largestoftwo.java
@@ -0,0 +1,19 @@
+public class largestoftwo {
+
+    public static void main(String[] args) {
+        
+        int a=100; int b=200; int c=300;
+        if(a>b && a>c)
+        {
+            System.out.println(a+"a is laegest number");
+        }
+        else if(b>c)
+        {
+            System.out.println(b+ "b is largest number");
+        }
+        else{
+            System.out.println(c+"C is largest number");
+        }
+
+    }
+}
