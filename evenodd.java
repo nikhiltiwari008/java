5c01bceaed895080848ef2ca8ed7346ac14bcd83
Nikhil Tiwari <69097170+nikhiltiwari008@users.noreply.github.com>
2022-11-09 14:19:47 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,12 @@
+public class evenodd {
+    public static void main(String[] args) {
+        
+        int a=23;
+        if(a%2==0){
+            System.out.println(a+"a is even number");
+        }
+        else{
+            System.out.println(a+"a is odd number");
+        }
+    }
+}
