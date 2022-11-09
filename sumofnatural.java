3536ff86794ed8dc2ea7a701193486b7d1f36375
Nikhil Tiwari <69097170+nikhiltiwari008@users.noreply.github.com>
2022-11-09 14:26:05 +0530

    Add files via upload

---
+++ sumofnatural.java
@@ -0,0 +1,13 @@
+public class sumofnatural {
+
+    public static void main(String[] args) {
+
+        int i,n=10,sum=0;
+        for(i=1;i<=n;i++){
+
+            sum=sum+i;
+           
+        }
+        System.out.print(sum);
+    }
+}
