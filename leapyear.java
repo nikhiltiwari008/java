605b8b51658331d56baffec17b2c61794781f24f
Nikhil Tiwari <69097170+nikhiltiwari008@users.noreply.github.com>
2022-11-09 14:23:59 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,19 @@
+public class leapyear {
+    public static void main(String[] args) 
+    {
+        int a=2014;
+        if(a%4==0 && a%100!=0 || a%400==0)
+        {
+            System.out.println(a+"a is leap year");
+        }
+        else{
+            System.out.println(a+"a is not a leap year");
+            }
+
+
+            int c=30;
+            System.out.println(c/3);
+            System.out.println(c%3);
+        }   
+      
+    }
